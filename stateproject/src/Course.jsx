0a4092707ff1d4d5e0e8563a89fb963ff2ee0c73
kubranuryavuz <nurkacm@gmail.com>
2024-12-09 13:27:01 +0300

    first commit

---
+++ stateproject/src/Course.jsx
@@ -0,0 +1,26 @@
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Ccsharp from "./images/ccsharp.png";
+import KompleWeb from "./images/kompleweb.jpg";
+
+
+const courseMap ={
+    Angular,
+    Bootstrap,
+    Ccsharp,
+    KompleWeb
+}
+
+
+function Course({courseName}) {
+    // console.log(Angular);
+    // console.log(courseName);
+    console.log(courseMap[courseName]);
+    return (  
+        <div>
+        <img src="{courseMap[courseName]}" alt="" />
+        </div>
+    );
+}
+
+export default Course;
